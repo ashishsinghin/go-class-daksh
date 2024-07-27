96f54006d3628ce3318ca1627a05f9f6d73f2459
Ashish Singh <ashishsingh21798@gmail.com>
2024-07-27 09:59:30 +0530

    go function, if , else, switch and defer

---
+++ for.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	sum := 0
+	// var i int
+	// for i = 0; i < 10; i++ {
+	// 	sum += i // sum = sum+i
+	// }
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
+}
