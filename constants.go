b0ce1a8ef775507eb485ab523b8a04d738237ea4
Ashish Singh <ashishsingh21798@gmail.com>
2024-07-27 09:18:36 +0530

    go basics, function and variables

---
+++ constants.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+const Pi = 3.14
+
+func main() {
+	const World = "world"
+	fmt.Println("Hello", World)
+	fmt.Println("HAppy", Pi, "day")
+
+	const Truth = true
+	fmt.Println("Go Rules?", Truth)
+}
